604f32bee0cf65949de0dbc4eb07ae0cb52cb6d3
Joshua-Talltree <73854373+Joshua-Talltree@users.noreply.github.com>
2020-11-10 11:12:24 -0600

    The First 'Real' assessment

--- solutions.js
+++ solutions.js
@@ -1,21 +1,100 @@
 "use strict";
 
-/**
- * Write your solutions here.
- *
- * **Note**: While normally it is good practice to wrap your javascript in an
- * immediately invoked function expression (iife), you should _not_ do that
- * here. If you do, the automated tests will not be able to see your functions.
- *
- * isGreaterThan
- * isLessThan
- * isEqualNumber
- * isEqual
- * isNot
- * calcAbs
- * calcCube
- * isTypeMatch
- * findLongestString
- * isFactor
- */
+
+ // * Write your solutions here.
+ // *
+ // * **Note**: While normally it is good practice to wrap your javascript in an
+ // * immediately invoked function expression (iife), you should _not_ do that
+ // * here. If you do, the automated tests will not be able to see your functions.
+ // *
+ // * isGreaterThan
+ // * isLessThan
+ // * isEqualNumber
+ // * isEqual
+ // * isNot
+ // * calcAbs
+ // * calcCube
+ // * isTypeMatch
+ // * findLongestString
+ // * isFactor
+
+
+// * isGreaterThan
+
+function isGreaterThan(num1, num2) {
+    if (num1 > num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * isLessThan
+
+function isLessThan(num1, num2) {
+    if (num1 <+ num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * isEqualNumber
+
+function isEqualNumber(num1, num2) {
+    if (num1 === num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * isEqual
+
+function isEqual(num1, num2) {
+    if (num1 == num2) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * isNot
+
+function isNot(num1, num2) {
+    if (!num1 && !num2 ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// * calcAbs
+
+function calcAbs(num1) {
+    if (num1 === num1 && null){
+        return true;
+    }
+}
+
+
+// * calcCube
+
+function calcCube(num1) {
+    if (num1 === null || NaN) {
+        return 0;
+    }
+}
+
+// * isTypeMatch
+
+function isMatchType(num1, num2) {
+    if (num1 === num2) {
+        return true;
+    } else if (num1 === !num2) {
+        return null;
+    }
+}
+
+
 
