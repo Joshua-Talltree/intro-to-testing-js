b9f9a554bf06326a41c6ecef952a1ac23b6ae659
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2020-11-12 15:12:25 -0600

    helloWorld Test's part 2

--- code.js
+++ code.js
@@ -3,10 +3,13 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "Hello, Jane!";
+function sayHello(input) {
+    if (input === "Hello") {
+        return "Say, Hello!";
+    } else if (input === "Jane") {
+        return "Hello, Jane!";
+    } else if (input === "Alex") {
+        return "Hello, Alex";
+    }
 }
 
-function sayHello(Jane) {
-    if ()
-}
